fb59f287a006414ea94eb5fc892d25278a286e75
Your Name <you@example.com>
2025-04-17 09:29:46 +0500

    EXAM

---
+++ Exam/ToDoList.Bll/Dtos/ToDoListGetDto.cs
@@ -0,0 +1,11 @@
+﻿namespace ToDoList.Bll.Entities;
+
+public class ToDoListGetDto
+{
+    public long ToDoItemId { get; set; }
+    public string Title { get; set; }
+    public string Discription { get; set; }
+    public bool IsCompleted { get; set; }
+    public DateTime CreatedAt { get; set; }
+    public DateTime DueDate { get; set; }
+}
